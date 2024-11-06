8fc44f4009df5666af54d7d6f361822f47613fcd
Sanket Rathod <118626260+sanketrathod07@users.noreply.github.com>
2024-11-06 21:12:03 +0530

    Update Chat.jsx height issue 2

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -101,7 +101,7 @@ const Container = styled.div`
     @media only screen and (max-width: 700px) {
       .showSidebarContact{
         width: 100vw;
-        height: 100vh;
+        height: 94vh;
       }
       .chatMobileStyleSHOW{
         display: none;
