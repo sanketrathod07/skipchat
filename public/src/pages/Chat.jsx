557ee473738070b30bcc72deb522c3d00b1cdf62
Sanket Rathod <118626260+sanketrathod07@users.noreply.github.com>
2024-11-06 18:19:31 +0530

    Update Chat.jsx

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -101,6 +101,7 @@ const Container = styled.div`
     @media only screen and (max-width: 700px) {
     .showSidebarContact{
       width: 100vw;
+      height: 100vh;
     }
     .chatMobileStyleSHOW{
       display: none;
